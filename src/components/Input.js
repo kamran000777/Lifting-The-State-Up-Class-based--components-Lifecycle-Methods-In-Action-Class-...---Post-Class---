c80825337c37c7cc05ed32ca07b58478a3e5f359
Kamran Ansari <96384339+kamran000777@users.noreply.github.com>
2022-06-08 18:17:51 +0530

    Create Input.js

---
+++ src/components/Input.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "./GlobalContext";
+
+const idGen = () => {
+  return Math.floor(Math.random() * 10000) + 1;
+};
+const Input = () => {
+  const [text, setText] = useState("");
+  const { addTodo } = useContext(GlobalContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const obj = {
+      id: idGen(),
+      text: text
+    };
+    addTodo(obj);
+  };
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <textarea
+          id="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit" id="btn">
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Input;
