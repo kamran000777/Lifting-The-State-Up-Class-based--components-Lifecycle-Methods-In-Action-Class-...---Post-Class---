b5b5f1e85dcbdd5eb68f232906f89666fd1b95f7
Kamran Ansari <96384339+kamran000777@users.noreply.github.com>
2022-06-08 18:17:02 +0530

    Create GlobalContext.js

---
+++ src/components/GlobalContext.js
@@ -0,0 +1,63 @@
+import React, { useReducer } from "react";
+
+export const GlobalContext = React.createContext();
+
+const initialState = {
+  lists: [
+    { id: 455, text: "Kamran" },
+    { id: 4575, text: "Kamran4" },
+    { id: 4875, text: "Kamran5" },
+    { id: 4545, text: "Kamran55" }
+  ]
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return { ...state, lists: [...state.lists, action.payload] };
+    case "delete":
+      return {
+        ...state,
+        lists: state.lists.filter((list) => list.id !== action.payload)
+      };
+    case "edit":
+      return {
+        ...state,
+        lists: state.lists.map((list) =>
+          list.id === action.id ? { ...list, text: action.payload } : list
+        )
+      };
+    default:
+      return;
+  }
+};
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  function addTodo(todo) {
+    dispatch({
+      type: "add",
+      payload: todo
+    });
+  }
+  function deleteTodo(id) {
+    dispatch({
+      type: "delete",
+      payload: id
+    });
+  }
+  function editTodo(index, text) {
+    dispatch({
+      type: "edit",
+      id: index,
+      payload: text
+    });
+  }
+  return (
+    <GlobalContext.Provider
+      value={{ lists: state.lists, addTodo, deleteTodo, editTodo }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
