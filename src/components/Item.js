35c707fe5ec3c7fb011d163786312d73f4feaa96
Kamran Ansari <96384339+kamran000777@users.noreply.github.com>
2022-06-08 18:18:12 +0530

    Create Item.js

---
+++ src/components/Item.js
@@ -0,0 +1,48 @@
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "./GlobalContext";
+
+const Item = ({ list }) => {
+  const [expand, setExpand] = useState(false);
+  const [input, setInput] = useState("");
+  const { deleteTodo, editTodo } = useContext(GlobalContext);
+  const changeHandler = (e) => {
+    const saveBtn = document.querySelector(".saveTask");
+    if (e.target.value !== "") {
+      setInput(e.target.value);
+    }
+  };
+  const clickHandler = () => {
+    editTodo(list.id, input);
+    setExpand(!expand);
+  };
+  return (
+    <>
+      <li>{list.text}</li>
+      <button className="delete" onClick={() => deleteTodo(list.id)}>
+        Delete
+      </button>
+      <button className="edit" onClick={() => setExpand(!expand)}>
+        Edit
+      </button>
+      {expand && (
+        <div>
+          <textarea
+            className="editTask"
+            value={input}
+            onChange={changeHandler}
+          />
+          <button
+            type="submit"
+            className="saveTask"
+            onClick={clickHandler}
+            disabled={input.length > 0 ? false : true}
+          >
+            Save
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Item;
